591329b218d93a1fa310c5e0e71b0ed9f6f97ef5
Mahdyar Entezami <90097342+MahdyarEn@users.noreply.github.com>
2021-12-25 18:05:04 +0330

    Change API Address

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://lightseen.ir/crypto/index.php";
+const BASE_URL = "https://nikan46.ir/api/currency.php";
 
 export const getCurrency = async () => {
   const response = await axios.get(BASE_URL);
